38b6335d4b276b97618407585811d8ff30f7277c
remarkablemark <remarkablemark@users.noreply.github.com>
2022-10-29 19:58:06 +0000

    Deploying to gh-pages from @ remarkablemark/parcel-typescript-path-alias-demo@2e2a6180ad0688052916bbbeebc77ad8db2d7b27 🚀

---
+++ index.34438762.js
@@ -0,0 +1,2 @@
+((...l)=>{console.log(...l)})("Hello world!");
+//# sourceMappingURL=index.34438762.js.map
---
+++ index.e0e2a71a.js
@@ -0,0 +1,2 @@
+((...l)=>{console.log(...l)})("Hello world!");
+//# sourceMappingURL=index.e0e2a71a.js.map
